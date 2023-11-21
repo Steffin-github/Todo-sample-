79f84b3564882c11beb204a6a276bc6efc817231
Steffin-github <149861970+Steffin-github@users.noreply.github.com>
2023-11-21 22:23:34 +0530

    first commit

---
+++ app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './unit/navbar/navbar.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, NavbarComponent, ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Day4';
+}
